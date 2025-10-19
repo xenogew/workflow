2f42e8ea6fb122cf1d305bc388a8ea3c8d612b93
Natta Wang <xenogew@gmail.com>
2025-10-20 07:34:21 +0900

    init vue for 'web'

---
+++ web/src/components/WeekChart.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="chart-container">
+    <canvas ref="chart"></canvas>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Chart from 'chart.js'
+
+const props = defineProps({
+  chartData: {
+    type: Array,
+    required: true,
+  },
+})
+
+onMounted(() => {
+  const config = {
+    type: 'bar',
+    data: {
+      labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+      datasets: [
+        {
+          data: props.chartData,
+        },
+      ],
+    },
+  }
+
+  new Chart(props.chartRef, config)
+})
+</script>
+
+<style scoped>
+.chart-container {
+  position: relative;
+  height: 100%;
+  width: 100%;
+}
+</style>
