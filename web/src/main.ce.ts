4de7d92ef29eeca36583927faf969a9f1f7fd957
Natta Wang <xenogew@gmail.com>
2025-10-22 01:40:13 +0900

    make draft of component-based with build outcome as custom-element for flexibly bundle into Thymeleaf

---
+++ web/src/main.ce.ts
@@ -0,0 +1,5 @@
+import { defineCustomElement } from "vue";
+import WeekChart from "@/components/WeekChart.vue";
+
+const weekChartElement = defineCustomElement(WeekChart);
+customElements.define("week-chart", weekChartElement);
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,24 +1,46 @@
-import { fileURLToPath, URL } from 'node:url'
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [
+    vue({
+      template: {
+        compilerOptions: {
+          isCustomElement: (tag) => tag.includes("-"),
+        },
+      },
+    }),
+    vueDevTools(),
+  ],
+  build: {
+    lib: {
+      entry: "src/main.ce.ts",
+      name: "MyVueCustomElement",
+      fileName: (format) => `my-customized.${format}.js`,
+    },
+    minify: false,
+    rollupOptions: {
+      output: {
+        format: "es",
+      },
+    },
+  },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
+      "/api": {
+        target: "http://localhost:8080",
         changeOrigin: true,
         secure: false,
         ws: true,
       },
     },
   },
-})
+});
