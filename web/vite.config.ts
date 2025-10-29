8c9fddf07b7b3536e55a405498155b2c91998b90
Natta Wang <xenogew@gmail.com>
2025-10-29 22:53:31 +0900

    add shadcn-vue

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,5 +1,6 @@
 import { fileURLToPath, URL } from "node:url";
 import { resolve } from "node:path";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
@@ -17,6 +18,7 @@ export default defineConfig({
         },
       },
     }),
+    tailwindcss(),
     vueDevTools(),
   ],
   build: {
